7aaa475f33153f5909c6e935ca90a1e1ad5310e1
Guilherme Rodrigues Costa <rodriguesguidev@gmail.com>
2024-02-21 21:58:19 -0300

    calc_media

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿float  nota1, nota2, nota3, media;
+
+while (true)
+{ 
+
+    Console.WriteLine("Irforme a 'Nota1':");
+    nota1 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Irforme a 'Nota2':");
+    nota2 = float.Parse(Console.ReadLine());
+    Console.WriteLine("Irforme a 'Nota3':");
+    nota3 = float.Parse(Console.ReadLine());
+
+    media = nota1 + nota2 + nota3/(3);
+
+    Console.WriteLine("A sua Média É:"  +  media.ToString("0.00"));
+
+
+    Console.WriteLine("Digite 'Sair' para encerrar ou 'ENTER' para  continuar:");
+    string opcao = Console.ReadLine();
+    if(opcao.ToLower() == "Sair")
+    {
+        break;
+    }
+}
